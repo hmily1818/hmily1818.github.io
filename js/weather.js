ee509cf5bfef4838e14950b8718ff77ee5bb2d89
hmily1818 <63196479+hmily1818@users.noreply.github.com>
2023-01-09 18:11:50 +0800

    Site updated: 2023-01-09 18:11:49

---
+++ js/weather.js
@@ -0,0 +1,21 @@
+WIDGET = {
+    "CONFIG": {
+      "modules": "01234",
+      "background": "1",
+      "tmpColor": "FFFFFF",
+      "tmpSize": "16",
+      "cityColor": "FFFFFF",
+      "citySize": "16",
+      "aqiColor": "FFFFFF",
+      "aqiSize": "16",
+      "weatherIconSize": "24",
+      "alertIconSize": "18",
+      "padding": "10px 10px 10px 10px",
+      "shadow": "0",
+      "language": "auto",
+      "fixed": "false",
+      "vertical": "top",
+      "horizontal": "left",
+      "key": "6af0298ff81c4b73961b5da0a5628b51"
+    }
+  }
